61e7466486a46c9ae3c925ff457c90827668eb03
nnorukaemeka <nnorukaemeka@gmail.com>
2025-09-23 00:30:43 +0100

    Added a reusable to_dict method- base mixin

--- database.py
+++ database.py
@@ -2,6 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
 from config import DATABASE_URL
 from sqlalchemy.exc import OperationalError
+from datetime import datetime
+from sqlalchemy.ext.declarative import declared_attr
+
 
 # Create PostgreSQL Engine
 try:
@@ -25,3 +28,23 @@ def get_db_session():
         yield db
     finally:
         db.close()
+
+
+# Create a base mixin that every SQLAlchemy model inherits from
+class BaseModelMixin:
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
+
+    def to_dict(self):
+        data = {}
+        for column in self.__table__.columns:
+            value = getattr(self, column.name)
+
+            # Convert datetime to ISO format
+            if isinstance(value, datetime):
+                value = value.isoformat()
+
+            data[column.name] = value
+        return data
+    
